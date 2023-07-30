beb41157060baaf6f88e6ca795d5a27c884266f3
mayurmehar003 <72783460+mayurmehar003@users.noreply.github.com>
2023-07-30 12:46:38 +0530

    throws(IOException)
    throws(IOException)

---
+++ Demo1.java
@@ -0,0 +1,27 @@
+package day1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class Demo1
+{
+public static void findfile() throws IOException
+{
+	File file=new File("mayur.txt");
+	FileInputStream f1=new FileInputStream(file);
+}
+
+public static void main(String[] args)
+{
+	try {
+	findfile();
+	}
+	catch(IOException e)
+	{
+		System.out.println("handled");
+	}
+
+}
+
+}
